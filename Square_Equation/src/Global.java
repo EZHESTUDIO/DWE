7d9d10796ee113687c25dfa7a23f11e7eb0b8f1d
EZHE STUDIO <ezhe-studio@ya.ru>
2015-05-31 23:51:59 +0300

    Evolution_3
    It's alliwe! Try it, eat it, by it!!!!111

--- Square_Equation/src/Global.java
+++ Square_Equation/src/Global.java
@@ -22,15 +22,16 @@ public class Global {
 		b = in.nextDouble();
 		System.out.println("Введите значение c");
 		c = in.nextDouble();	
-		System.out.printf("Уравнение имеет вид: %dx^2 + %dx + %d = 0 \n", a, b, c );
+		System.out.printf("Уравнение имеет вид: %.2fx^2 + %.2fx + %.2f = 0 \n", a, b, c );
 	
 		D = b*b + 4*a*c;
 		System.out.println("Значение дискриминанта равно D = " +D);
 		
 		sqrtD = Math.sqrt(D);
 		x1 = (-b + sqrtD)/(2*a); 
+		x2 = (-b - sqrtD)/(2*a); 	
 		
-//		x1 = ( -b - sqrt(D) )/( 2 * a );
+		System.out.printf("Корни уравнения = %.2f и %.2f", x1, x2 );
 		
 		
 
