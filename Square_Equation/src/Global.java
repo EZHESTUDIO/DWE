a892adebf6e89a331f9a0c00d02c34393aedce70
EZHE STUDIO <ezhe-studio@ya.ru>
2015-05-31 16:54:29 +0300

    Evolution...

--- Square_Equation/src/Global.java
+++ Square_Equation/src/Global.java
@@ -1,11 +1,23 @@
 
 public class Global {
 	public static void main( String args[] ) 
-	throws java.io.IOException {
-	int x, y, a, b, c;
-	System.out.println("я решаю квадратные уравнени€!!!1111");
-
+		throws java.io.IOException {
+		int x, a, b, c;
+		System.out.println("я решаю квадратные уравнени€!!!1111");
+		System.out.println("”равнение имеет вид: ax^2 + bx + c = 0");
+		System.out.println("x - искома€ величина, a, b, c - константы.");
+		System.out.println("¬ведите значение a");
+		a = (int) System.in.read() - 48;
+		System.out.println("¬ведите значение b");
+		b = (int) System.in.read() - 48;
+		System.out.println("¬ведите значение c");
+		c = (int) System.in.read() - 48;
+		System.out.print("”равнение имеет вид: " + a );
 	
+
+		//System.out.println("«начение x равно " +x);
+		
+
 	}
 }
 	
