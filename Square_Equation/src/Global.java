eafc0a47d0458606dd328076dceed14c4209bbe6
EZHE STUDIO <ezhe-studio@ya.ru>
2015-05-31 16:12:06 +0300

    First_Files

---
+++ Square_Equation/src/Global.java
@@ -0,0 +1,11 @@
+
+public class Global {
+	public static void main( String args[] ) 
+	throws java.io.IOException {
+	int x, y, a, b, c;
+	System.out.println("Я решаю квадратные уравнения!!!1111");
+
+	
+	}
+}
+	
